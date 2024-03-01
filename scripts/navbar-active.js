63b1b72cbf09fc3a0ca54ee74f74ca7f544c504d
Ryan <ryanmitcham512@gmail.com>
2024-03-01 19:52:50 +0000

    Adding projects page

--- scripts/navbar-active.js
+++ scripts/navbar-active.js
@@ -1,7 +1,11 @@
 $(function(){
     $(".navbar li a").each(function(){
         var $this = $(this);
-        if("/" + $this.attr("href") == location.pathname){
+
+        var href_layers = $this.attr("href").split("/").filter(function (n) {return n != ""; });
+        var location_layers = location.pathname.split("/").filter(function (n) {return n != ""; });
+
+        if(location_layers.indexOf(href_layers[0]) !== -1){
             $this.addClass("navbar-active");
         }
     })
--- scripts/navbar-load.js
+++ scripts/navbar-load.js
@@ -1,3 +1,3 @@
 $(function(){
-    $("nav").load("navbar.html");
+    $("nav").load("/navbar.html");
 });
